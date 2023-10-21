64492881d9aa49745a09b10b92fa982115b2fb6e
YoneiAtsushi <YoneiAtsushi@users.noreply.github.com>
2023-10-21 12:14:29 +0000

    [Finish]log inページの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :books, only: [:new, :index, :show, :edit]
   resources :users, only: [:edit, :show, :index]
-  devise_for :users
+ 
   root to: 'homes#top'
+  get "/homes/about" => "homes#about", as: "about"
 end
