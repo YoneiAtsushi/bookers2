dd171be99f48cce88653c625c9a8383b89a67252
YoneiAtsushi <YoneiAtsushi@users.noreply.github.com>
2023-10-24 18:32:55 +0000

    [Re start]

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import "channels"
-
 import "jquery";
 import "popper.js";
 import "bootstrap";
