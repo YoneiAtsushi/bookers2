ff5b9c98179d83a6a956605042d8019677e7b1a2
YoneiAtsushi <YoneiAtsushi@users.noreply.github.com>
2023-10-30 14:19:33 +0000

    [Start]いいね機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,11 @@ class Book < ApplicationRecord
 
 has_one_attached :image
 belongs_to :user
+has_many :favorites, dependent: :destroy
+
+def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
   validates :title, presence: true
   validates :body, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   resources :users, only: [:edit, :show, :index, :update]
+  resource :favorite, only: [:create, :destroy]
 
-  root to: 'homes#top'
+  root to: "homes#top"
   get "/home/about" => "homes#about", as: "about"
 end
