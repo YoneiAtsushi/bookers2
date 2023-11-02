782fedaeb4002d90e7e303f3163b2cdb6c467dbd
YoneiAtsushi <YoneiAtsushi@users.noreply.github.com>
2023-11-02 09:04:29 +0000

    [start]フォロー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  resources :users, only: [:edit, :show, :index, :update]
-  
+
+  resources :users, only: [:edit, :show, :index, :update] do
+  resource :relationships, only: [:create, :destroy]
+   get 'followings' => 'relationships#followings', as: 'followings'
+   get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books do
   resources :book_comments, only: [:create, :destroy]
   resource :favorite, only: [:create, :destroy]
