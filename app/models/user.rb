b3e0908010588295506ce380864a630b714e3f96
YoneiAtsushi <YoneiAtsushi@users.noreply.github.com>
2023-11-04 14:57:02 +0000

    [Finish]フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
+  # attachment :profile_image
   
    has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
    has_many :followers, through: :reverse_of_relationships, source: :follower
